d167c084969fbb620f30c9bb7fafe651fc298195
muskanYadav <166051734+Mvskywert345@users.noreply.github.com>
2025-01-26 19:49:45 +0530

    Add files via upload

---
+++ Music_ac/src/components/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero container' id="hero"> 
+      <div className='hero-text'>
+        <h1> We Ensure Music Is accessible to everyone</h1>
+        <p>Unlock Your Musical Journey – Anytime, Any Age!</p>
+        <button className='btn'>Explore more</button>
+       
+      </div>
+    </div>
+  )
+}
+
+export default Hero
