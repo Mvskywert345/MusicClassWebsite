c859337b54512f6baa271453666e631dbe3a44b9
muskanY2 <mern.muskanydv@gmail.com>
2025-02-07 15:31:48 +0530

    updated hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './Hero.css'
 
+
+
 const Hero = () => {
   return (
     <div className='hero container' id="hero"> 
       <div className='hero-text'>
         <h1> We Ensure Music Is accessible to everyone</h1>
         <p>Unlock Your Musical Journey – Anytime, Any Age!</p>
-        <button className='btn'>Explore more</button>
-       
+
+        <button className="btn" onClick={() => document.getElementById("courses").scrollIntoView({ behavior: "smooth" })}>
+  Explore More
+</button>
+
       </div>
     </div>
   )
